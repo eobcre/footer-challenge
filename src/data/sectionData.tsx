80b886a3c8a9b344eff6531d6150c9ab74c82cc3
eobcre <eobcre@gmail.com>
2023-08-25 11:32:05 -0400

    created data

---
+++ src/data/sectionData.tsx
@@ -0,0 +1,24 @@
+export const sectionData = [
+  {
+    title: 'Explore',
+    items: [
+      { name: 'New uploads', src: '#' },
+      { name: 'Popular designs', src: '#' },
+      { name: 'Contributors', src: '#' },
+    ],
+  },
+  {
+    title: 'Site',
+    items: [
+      { name: 'License', src: '#' },
+      { name: 'Articles', src: '#' },
+      { name: 'About', src: '#' },
+      { name: 'Support', src: '#' },
+      { name: 'Sponsors', src: '#' },
+    ],
+  },
+  {
+    title: 'Other products',
+    items: [{ name: 'Boardme', src: '#' }],
+  },
+];
